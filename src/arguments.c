4ad9173a6aabb95cf65081cdde6a6d2cb39e8c64
gabrielhamel <gabriel.hamel@epitech.eu>
2019-05-09 02:27:47 +0200

    Removed quadruple indentation

--- src/arguments.c
+++ src/arguments.c
@@ -15,26 +15,31 @@ void usage(const char *name)
     "[--printBroadcast] [--printSpoof] mac\n", name);
 }
 
+static int parse_one_arg(arg_t *arg, char **av, int idx)
+{
+    if (idx == 1) {
+        arg->src_ip = av[idx];
+        if (is_valid_ip(av[idx]))
+            return (RETURN_FAILURE);
+    }
+    if (idx == 2) {
+        arg->dest_ip = av[idx];
+        if (is_valid_ip(av[idx]))
+            return (RETURN_FAILURE);
+    }
+    if (idx == 3) {
+        arg->iface = av[idx];
+        if (is_valid_interface(av[idx]))
+            return (RETURN_FAILURE);
+    }
+    return (RETURN_SUCCESS);
+}
+
 static int parse_ip_iface(arg_t *arg, char **av)
 {
     for (int i = 1; i < 5; i++)
-        switch (i) {
-            case 1:
-                arg->src_ip = av[i];
-                if (is_valid_ip(av[i]))
-                    return (RETURN_FAILURE);
-            break;
-            case 2:
-                arg->dest_ip = av[i];
-                if (is_valid_ip(av[i]))
-                    return (RETURN_FAILURE);
-            break;
-            case 3:
-                arg->iface = av[i];
-                if (is_valid_interface(av[i]))
-                    return (RETURN_FAILURE);
-            break;
-        }
+        if (parse_one_arg(arg, av, i) == RETURN_FAILURE)
+            return (RETURN_FAILURE);
     return (RETURN_SUCCESS);
 }
 
